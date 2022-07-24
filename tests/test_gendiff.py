bdd9f09a4907c523b8855fc34b28e7676d1f64e3
SevaErsh <erschidvs@yandex.ru>
2022-07-24 19:08:49 +0300

    edit tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,6 +2,8 @@ import json
 import gendiff as g
 
 
-def test_stringify():
-    test_json = json.load(open('tests/fixtures/file1.json'))
-    assert g.stringify(test_json) == '{\n host: hexlet.io\n timeout: 50\n proxy: 123.234.53.22\n follow: False\n}'
+def test_generate_diff():
+    first_file = 'tests/fixtures/file1.json'
+    second_file = 'tests/fixtures/file2.json'
+    with open('tests/fixtures/expected.txt', 'r') as expected:
+        assert g.generate_diff(first_file, second_file) == expected.read()
