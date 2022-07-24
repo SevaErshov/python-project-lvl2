15d6fd8335abe247a99e142b929355b1b6519de0
SevaErsh <erschidvs@yandex.ru>
2022-07-24 14:54:00 +0300

    edit tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,5 +3,5 @@ import gendiff as g
 
 
 def test_stringify():
-    test_json = json.load(open('gendiff/tests/fixtures/file1.json'))
+    test_json = json.load(open('tests/fixtures/file1.json'))
     assert g.stringify(test_json) == '{\n host: hexlet.io\n timeout: 50\n proxy: 123.234.53.22\n follow: False\n}'
